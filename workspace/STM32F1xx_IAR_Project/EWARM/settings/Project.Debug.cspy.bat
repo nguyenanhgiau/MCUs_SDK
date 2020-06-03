be21a8a428c71effb1bef6995f08d0d658e7f37a
anh_giau <nguyenanhgiau1008@gmail.com>
2020-06-03 10:07:34 +0700

    add empty project for STM32F10x family

--- workspace/STM32F1xx_IAR_Project/EWARM/settings/Project.Debug.cspy.bat
+++ workspace/STM32F1xx_IAR_Project/EWARM/settings/Project.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\nguye\Desktop\MCUs_SDK\workspace\STM32F1xx_IAR_Project\EWARM\settings\Project.Debug.general.xcl" --backend -f "C:\Users\nguye\Desktop\MCUs_SDK\workspace\STM32F1xx_IAR_Project\EWARM\settings\Project.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\Giau\Desktop\MCUs_SDK\workspace\STM32F1xx_IAR_Project\EWARM\settings\Project.Debug.general.xcl" --backend -f "C:\Users\Giau\Desktop\MCUs_SDK\workspace\STM32F1xx_IAR_Project\EWARM\settings\Project.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\nguye\Desktop\MCUs_SDK\workspace\STM32F1xx_IAR_Project\EWARM\settings\Project.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\nguye\Desktop\MCUs_SDK\workspace\STM32F1xx_IAR_Project\EWARM\settings\Project.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\Giau\Desktop\MCUs_SDK\workspace\STM32F1xx_IAR_Project\EWARM\settings\Project.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Giau\Desktop\MCUs_SDK\workspace\STM32F1xx_IAR_Project\EWARM\settings\Project.Debug.driver.xcl" 
 
 @echo off 
 :end
