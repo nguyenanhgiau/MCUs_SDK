75b2c3ca371cec8c62e9c8cfa564fec2097fdf7d
anh_giau <nguyenanhgiau1008@gmail.com>
2020-06-09 13:58:08 +0700

    add init hardware com for stm8l

--- workspace/STM8Lxxx_IAR_Project/EWSTM8/main.c
+++ workspace/STM8Lxxx_IAR_Project/EWSTM8/main.c
@@ -35,7 +35,6 @@
 #include "app_main.h"
 #include "dbg.h"
 #include "port_mcu.h"
-#include "serial.h"
 
 /* Private defines -----------------------------------------------------------*/
 /* Private variable ----------------------------------------------------------*/
@@ -115,17 +114,44 @@ static bool BUTTON_bRead(void)
 
 static void uart_drv_send(uint8_t u8TxByte)
 {
-    
+    /* Write a character to the USART */
+    USART_SendData8(USART1, u8TxByte);
+    /* Loop until the end of transmission */
+    while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
 }
 
 static uint8_t uart_drv_receive(void)
 {
-    return 0;
+    /* Loop until the Read data register flag is SET */
+    while (USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET);
+    return USART_ReceiveData8(USART1);
 }
 
 static void uart_initialize(void)
 {
-    
+    /* USART configured as follow:
+        - BaudRate = 115200 baud  
+        - Word Length = 8 Bits
+        - One Stop Bit
+        - No parity
+        - Receive and transmit enabled
+        - USART Clock disabled
+    */
+    /* Enable USART clock */
+    CLK_PeripheralClockConfig((CLK_Peripheral_TypeDef)CLK_Peripheral_USART1, ENABLE);
+  
+    /* Configure USART Tx as alternate function push-pull  (software pull up)*/
+    GPIO_ExternalPullUpConfig(GPIOC, GPIO_Pin_3, ENABLE);
+
+    /* Configure USART Rx as alternate function push-pull  (software pull up)*/
+    GPIO_ExternalPullUpConfig(GPIOC, GPIO_Pin_2, ENABLE);
+
+    /* USART configuration */
+    USART_Init(USART1, (uint32_t)115200,
+               USART_WordLength_8b,
+               USART_StopBits_1,
+               USART_Parity_No,
+               (USART_Mode_TypeDef)(USART_Mode_Tx | USART_Mode_Rx));
 }
 
 static void led_initialize(void)
